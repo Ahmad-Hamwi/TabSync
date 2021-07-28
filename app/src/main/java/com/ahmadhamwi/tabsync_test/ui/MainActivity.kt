0f902a9bf20bde2556003cbc8c3bfd2d681b97bf
Ahmad-Hamwi <ahmadhamwi999@gmail.com>
2021-07-28 20:58:07 +0300

    Adding Mediator

--- app/src/main/java/com/ahmadhamwi/tabsync_test/ui/MainActivity.kt
+++ app/src/main/java/com/ahmadhamwi/tabsync_test/ui/MainActivity.kt
@@ -5,11 +5,58 @@ import android.os.Bundle
 import com.ahmadhamwi.tabsync_test.R
 import com.ahmadhamwi.tabsync_test.ui.model.Category
 import com.ahmadhamwi.tabsync_test.ui.model.Item
+import com.ahmadhamwi.tabsync_test.util.TabbedListMediator
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.recyclerView
 import kotlinx.android.synthetic.main.item_category.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val categories = mutableListOf(
+        Category(
+            "Category 1",
+            Item("Item 1"),
+            Item("Item 2"),
+            Item("Item 3"),
+            Item("Item 4"),
+            Item("Item 5"),
+            Item("Item 6")
+        ),
+        Category(
+            "Category 2",
+            Item("Item 1"),
+            Item("Item 2"),
+            Item("Item 3"),
+            Item("Item 4"),
+        ),
+        Category(
+            "Category 3",
+            Item("Item 1"),
+            Item("Item 2"),
+            Item("Item 3"),
+            Item("Item 4"),
+            Item("Item 5"),
+            Item("Item 6"),
+            Item("Item 7"),
+            Item("Item 8"),
+        ),
+        Category(
+            "Category 4",
+            Item("Item 1"),
+            Item("Item 2"),
+            Item("Item 3"),
+            Item("Item 4"),
+            Item("Item 5"),
+            Item("Item 6")
+        ),
+        Category(
+            "Category 5",
+            Item("Item 1"),
+            Item("Item 2"),
+            Item("Item 4"),
+            Item("Item 5"),
+        ),
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,64 +64,20 @@ class MainActivity : AppCompatActivity() {
 
         initTabLayout()
         initRecycler()
+        initMediator()
     }
 
     private fun initTabLayout() {
-        tabLayout.addTab(tabLayout.newTab().setText("Category 1"))
-        tabLayout.addTab(tabLayout.newTab().setText("Category 2"))
-        tabLayout.addTab(tabLayout.newTab().setText("Category 3"))
-        tabLayout.addTab(tabLayout.newTab().setText("Category 4"))
-        tabLayout.addTab(tabLayout.newTab().setText("Category 5"))
+        for (category in categories) {
+            tabLayout.addTab(tabLayout.newTab().setText(category.name))
+        }
     }
 
     private fun initRecycler() {
+        recyclerView.adapter = CategoriesAdapter(this, categories)
+    }
 
-        val listOfCategories = mutableListOf(
-            Category(
-                "Category 1",
-                Item("Item 1"),
-                Item("Item 2"),
-                Item("Item 3"),
-                Item("Item 4"),
-                Item("Item 5"),
-                Item("Item 6")
-            ),
-            Category(
-                "Category 2",
-                Item("Item 1"),
-                Item("Item 2"),
-                Item("Item 3"),
-                Item("Item 4"),
-            ),
-            Category(
-                "Category 3",
-                Item("Item 1"),
-                Item("Item 2"),
-                Item("Item 3"),
-                Item("Item 4"),
-                Item("Item 5"),
-                Item("Item 6"),
-                Item("Item 7"),
-                Item("Item 8"),
-            ),
-            Category(
-                "Category 4",
-                Item("Item 1"),
-                Item("Item 2"),
-                Item("Item 3"),
-                Item("Item 4"),
-                Item("Item 5"),
-                Item("Item 6")
-            ),
-            Category(
-                "Category 5",
-                Item("Item 1"),
-                Item("Item 2"),
-                Item("Item 4"),
-                Item("Item 5"),
-            ),
-        )
-
-        recyclerView.adapter = CategoriesAdapter(this, listOfCategories)
+    private fun initMediator() {
+        TabbedListMediator(recyclerView, tabLayout, categories.indices.toList()).attach()
     }
 }
